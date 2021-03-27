b6841a88ff542e03ef022800cb1e89443d678367
yuredev <yurematias@ufrn.edu.br>
2021-03-27 18:28:56 -0300

    feature(frontend): Initiate frontend application

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SignIn from './screens/SignIn';
+
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <>
+      <SignIn />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
---
+++ web/src/styles/global.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const backgroundColor = '#312E38';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+    color: #f4ede8;
+  }
+
+  body {
+    background-color: ${backgroundColor};
+    --webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto Slab', serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+`
+
+
+
+
